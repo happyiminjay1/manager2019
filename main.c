fe80276644b85d7cc91d0c7e4680d8e8949804d5
happyiminjay1 <21800511@handong.edu>
2019-04-08 01:34:33 +0900

    change in promptmode

--- main.c
+++ main.c
@@ -5,18 +5,37 @@ int main(int argc, char* argv[]) {
   LOGIN* userlist[100]; // 사용자목록 포인터 배열 (최대 100)
   int is_login=0; // 로그인 여부 (0 NO, 1 Yes)
   int menu_id;
+  char command[256];
   if (argc != 2) {
     printf("Usage : manager <datafile>\n");
     return 0;
   }
   int count = load_file(userlist, argv[1]);
+  int firstcount = count;
+  printf("> Welcome!!\n");
   while(1){
-    if(is_login==0) is_login = login(userlist, count);
-    menu_id = ask_menu(is_login); // 현재 로그인 되었는지의 여부를 파라미터로 알려야 한다.
-    if(menu_id==1) join(userlist, count);
-    else if (menu_id==2) is_login = login(userlist, count);
-    else if (menu_id==3) logout(&is_login);
-    else break;
+	if(is_login==0) 
+	{
+		printf("> ");
+		scanf("%s",command);
+		if(!strcmp(command,"login"))
+			is_login = login(userlist,count);
+		else if(!strcmp(command,"join"))
+			count = join(userlist,count);	
+		else if(!strcmp(command,"list"))
+			list(userlist,count);
+		else if(!strcmp(command,"exit"))
+			break;
+		else printf("No such command!\n");
+	}
+	else
+	{
+		printf("# ");
+		scanf("%s",command);
+		if(!strcmp(command,"logout"))
+				logout(&is_login);
+		else printf("%s\n",command);
+	}
   }
   save_file(userlist, count, argv[1]);
   return 0;
--- user.c
+++ user.c
@@ -2,18 +2,41 @@
 
 int load_file(LOGIN* list[], char* filename){
   int count=0;
-  FILE *datafile = fopen(filename, "r");
+ 
+  FILE * datafile = fopen(filename, "r");
+  if(datafile == NULL)
+  {
+	printf("%s file not exist! make anyway? (Yes 1, No 2) >> ",filename);
+	int filemake;
+	scanf("%d",&filemake);
+	if(filemake == 1)
+	{
+		datafile = fopen(filename,"w");
+		fclose(datafile);
+		datafile = fopen(filename,"r");
+	}
+	else
+	{
+		printf("exit");
+		exit(0);
+	}
+  }
   while(!feof(datafile)){
-    list[count]=(LOGIN*)malloc(sizeof(LOGIN));
+    
+	list[count]=(LOGIN*)malloc(sizeof(LOGIN));
     fscanf(datafile,"%s %s",list[count]->id,list[count]->password);
     count++;
   }
-  printf("%d records read!\n",count);\
+#ifdef DEBUG_MODE
+  printf("DEBUG>>");
+  printf("%d\n",count);
+#endif
+  printf("%d records read!\n",count-1);\
   fclose(datafile);
-  return count;
+  return count-1;
 }
 
-void join(LOGIN* list[], int count){
+int join(LOGIN* list[], int count){
   char id[20], pass[20];
   while(1){
     printf("Enter new user id >> ");
@@ -22,6 +45,10 @@ void join(LOGIN* list[], int count){
     for(int i=0;i<count;i++){
       if(strcmp(id, list[i]->id)==0) {
         dup=1; break;
+#ifdef DEBUG_MODE
+  printf("DEBUG>>");
+  printf("%d\n",count);
+#endif
       }
     }
     if(dup==1){
@@ -34,9 +61,15 @@ void join(LOGIN* list[], int count){
       strcpy(list[count]->id, id);
       strcpy(list[count]->password, pass);
       printf("New user added!\n");
+#ifdef DEBUG_MODE
+  printf("DEBUG>>");
+  printf("%d\n",count);
+#endif
+	  return ++count;
       break;
     }
   }
+  return count;
 }
 
 int login(LOGIN* list[], int count){
@@ -48,12 +81,16 @@ int login(LOGIN* list[], int count){
     if(strcmp(id, list[i]->id)==0) {
       dup=1;
       found = i;
+#ifdef DEBUG_MODE
+  printf("DEBUG>>");
+  printf("%d\n",found);
+#endif
       break;
     }
   }
   if(dup!=1){
     printf("No such user!!\n");
-    return -1;
+    return 0;
   }
   else{
     printf("Enter password >> ");
@@ -82,3 +119,11 @@ void save_file(LOGIN* list[], int count, char* filename){
   printf("%s Saved!\n", filename);
   fclose(datafile);
 }
+
+void list(LOGIN * list[], int count)
+{
+	printf("USER list (id/password)\n");
+	for(int i = 0; i<count; i++)
+		printf("[%d] %s / %s\n",i+1,list[i]->id,list[i]->password);
+}
+
